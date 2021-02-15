ef9ad922c096219610d119516f47b3fcfa1698c0
Laura <laura.saldarriagah@gmail.com>
2021-02-15 13:23:15 -0500

    Componente Button y clickHandler

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,63 @@
 //importación
 import React from "react";
-import "./App.css";
+import Button from "./components/Button";
 import Result from "./components/Result";
+
+import "./App.css";
 
 //Generación de la función del componente. Función flecha o arrow function
 const App = () => {
   console.log("Renderización de la App");
+
+  const clickHandlerFunction = text =>{//si es un solo parámetro no se suele poner paréntesis
+    console.log(text)
+  }
   //lo que ejecuta la función
   return (
     <main className="react-calculator">
       <Result></Result>
       <div className="numbers">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>0</button>
+        <Button
+          text="1"
+          clickHandler={clickHandlerFunction}>
+        </Button>
+        <Button
+          text="2"
+          clickHandler={clickHandlerFunction}>
+        </Button>
+        <Button
+          text="3"
+          clickHandler={clickHandlerFunction}>
+        </Button>
+        <Button
+          text="4"
+          clickHandler={clickHandlerFunction}>
+        </Button>
+        <Button
+          text="5"
+          clickHandler={clickHandlerFunction}>
+        </Button>
+        <Button
+          text="6"
+          clickHandler={clickHandlerFunction}>
+        </Button>
+        <Button
+          text="7"
+          clickHandler={clickHandlerFunction}>
+        </Button>
+        <Button
+          text="8"
+          clickHandler={clickHandlerFunction}>
+        </Button>
+        <Button
+          text="9"
+          clickHandler={clickHandlerFunction}>
+        </Button>
+        <Button
+          text="0"
+          clickHandler={clickHandlerFunction}>
+        </Button>
+      
       </div>
       <div className="functions">
         <button>Clear</button>
