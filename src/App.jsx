b1a4a3ed8a5652ca93584a20c159bd73dd78037c
Laura <laura.saldarriagah@gmail.com>
2021-03-13 16:32:57 -0500

    First use of "useState"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //importación
-import React from "react";
+import React, { useState } from "react";
 import MathOperations from "./components/MathOperations";
 import Numbers from "./components/Numbers";
 import Functions from "./components/Functions";
@@ -9,13 +9,25 @@ import "./App.css";
 
 //Generación de la función del componente. Función flecha o arrow function
 const App = () => {
+  const arrayTextoModificaTexto = useState(""); //valor inicial
+
+  //const arrayTextoModificaTexto => ["hola",funcion]
+  //1er posición: valor (que inicialmente es el valor por defecto)
+  const texto = arrayTextoModificaTexto[0];
+
+  //2da posición: función que permite modificar el valor por defecto
+  const funcionModificaTexto = arrayTextoModificaTexto[1];
+
   console.log("Renderización de la App");
   //lo que ejecuta la función
   return (
     <main className="react-calculator">
-      <Result></Result>
+      <Result value={texto}></Result>
       <Numbers
-        onClickNumber={(number) => console.log("Click en number", number)}
+        onClickNumber={(number) => {
+          console.log("Click en number", number);
+          funcionModificaTexto(number);
+        }}
       ></Numbers>
       <Functions
         onContentClear={() => console.log("Content Clear: ")}
